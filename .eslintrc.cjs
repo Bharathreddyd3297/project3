6cb6a39a3d98e8639bec50167f25963249759bd6
Bharath <bharath.reddy329@gmail.com>
2025-12-29 19:49:20 +0530

    C4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,              // allow describe/it/expect
   },
   extends: [
     'eslint:recommended',
@@ -16,5 +17,8 @@ module.exports = {
       version: 'detect',
     },
   },
-  rules: {},
+  rules: {
+    'react/react-in-jsx-scope': 'off',   // modern React doesn't need React import
+    'no-undef': 'off'                    // avoids describe/it errors in tests
+  },
 };
